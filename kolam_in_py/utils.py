e49f2070cf3501c35ec67928f03d18ac20f54f8f
sham-404 <shamsujith4@gmail.com>
2025-10-01 12:31:18 +0530

    Added Button class

--- kolam_in_py/utils.py
+++ kolam_in_py/utils.py
@@ -32,3 +32,69 @@ class Colors:
     LIME = (0, 255, 0)
     SKY_BLUE = (135, 206, 235)
     GOLD = (255, 215, 0)
+
+
+class Button:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        width: float,
+        height: float,
+        text="default",
+        one_press=False,
+        border_radius=5,
+        font: str | None = None,
+        font_size=20,
+    ):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text
+        self.one_press = one_press
+        self.border_radius = border_radius
+        self.colors = {
+            "normal": Colors.TEAL,
+            "hover": Colors.CYAN,
+            "pressed": Colors.DARK_GREEN,
+        }
+        self.font = pygame.font.SysFont(font, font_size)
+        self.pressed = False
+
+    def check_click(self, event):
+        action = False
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(mouse_pos):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.pressed = True
+                    if self.one_press:
+                        action = True
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1 and self.pressed:
+                    self.pressed = False
+                    if not self.one_press:
+                        action = True
+
+        else:
+            if self.pressed:
+                self.pressed = False
+
+        return action
+
+    def draw(self, screen):
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.pressed:
+            color = self.colors["pressed"]
+
+        elif self.rect.collidepoint(mouse_pos):
+            color = self.colors["hover"]
+
+        else:
+            color = self.colors["normal"]
+
+        pygame.draw.rect(screen, color, self.rect, border_radius=self.border_radius)
+        text_surf = self.font.render(self.text, True, "white")
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        screen.blit(text_surf, text_rect)
